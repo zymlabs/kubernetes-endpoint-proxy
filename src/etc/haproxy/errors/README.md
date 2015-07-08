580a6c3b1a47e10f1df8a50fa1b71d4ea73c09ee
Geoffrey Tran <geoffrey.tran@gmail.com>
2015-07-08 17:57:48 -0500

    Customized error pages for better debugging

--- src/etc/haproxy/errors/README.md
+++ src/etc/haproxy/errors/README.md
@@ -4,3 +4,7 @@ NGINX Error Pages
 The error pages located in this directory are generated using
 [Better Error Pages](https://better-error-pages.statuspage.io/).
 Use their editor to customize the pages to your project.
+
+Also appended is an HTML comment at the end of error pages to allow easier debugging in a complex environment.
+
+    <!-- Kubernetes Endpoint Proxy ${HOSTNAME} on ${HOST} -->
--- src/main.sh
+++ src/main.sh
@@ -11,22 +11,29 @@ export ETCD_PROTOCOL
 export ETCD_HOST
 export ETCD_PORT
 
+# Generate configuration by looping through .dist files in /etc/nginx/conf.d/default.conf
+for f in /etc/haproxy/errors/*.html
+do
+    # Replace ${VAR} with variables set in environment.
+    perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg' $f > $f
+done
+
 if [ -z "$KUBERNETES_ENDPOINT" ]; then
     echo "Environment variable KUBERNETES_ENDPOINT must be set with the Kubernetes service id"
     exit 1
 fi
 
-echo "[mesos-kubernetes-proxy] Starting Mesos Kubernetes Proxy..."
+echo "[kubernetes-endpoint-proxy] Starting Mesos Kubernetes Proxy..."
 
 # Loop until confd has updated the haproxy config
 until confd -onetime -node $ETCD_SCHEME://$ETCD_HOST:$ETCD_PORT -config-file /etc/confd/conf.d/haproxy.toml; do
-  echo "[mesos-kubernetes-proxy] Waiting for confd to refresh haproxy.cfg"
+  echo "[kubernetes-endpoint-proxy] Waiting for confd to refresh haproxy.cfg"
   sleep 5
 done
 
 # Run confd in the background to watch the upstream servers
 confd -interval 10 -node $ETCD_SCHEME://$ETCD_HOST:$ETCD_PORT -config-file /etc/confd/conf.d/haproxy.toml &
-echo "[mesos-kubernetes-proxy] confd is listening for changes on etcd..."
+echo "[kubernetes-endpoint-proxy] confd is listening for changes on etcd..."
 
-echo "[mesos-kubernetes-proxy] Starting HAProxy..."
+echo "[kubernetes-endpoint-proxy] Starting HAProxy..."
 exec /usr/bin/supervisord
